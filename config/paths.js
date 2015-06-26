d949c9ffec90bf957f00c2094a3de5412fa23b95
Evan Turner <evturn@gmail.com>
2015-06-26 11:08:44 -0400

    reconfigured directories and less precompresser

---
+++ config/paths.js
@@ -0,0 +1,14 @@
+module.exports = {
+	less: {
+		src: 'assets/less/*.less',
+		watch: 'assets/less/**/*.less',
+		dest: 'dist/css',
+		filename: 'less.css'
+	},
+	css: {
+
+	},
+	js: {
+
+	}
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,36 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    gless = require('gulp-less'),
-    watch = require('gulp-watch'),
-    notify = require('gulp-notify');
+    g = require('gulp-load-plugins')();
+
+var plunger = require('./config/gulp-error-handler');
+var paths = require('./config/paths');
 
 gulp.task('default', ['less']);
 
 gulp.task('less', function() {
-  return gulp.src('assets/css/less/**/*.less')
-    .pipe(watch('assets/css/less/**/*.less'))
-    .pipe(gless())
-    .pipe(gulp.dest('assets/css'));
+  return gulp.src(paths.less.src)
+    .pipe(g.plumber(plunger))
+    .pipe(g.less())
+    .pipe(g.rename(paths.less.filename))
+    .on('error', function (err) {
+        gutil.log(err);
+        g.notify(err);
+        this.emit('end');
+    })
+    .pipe(g.autoprefixer({
+      browsers: [
+	      '> 1%',
+	      'last 2 versions',
+	      'firefox >= 4',
+	      'safari 7',
+	      'safari 8',
+	      'IE 8',
+	      'IE 9',
+	      'IE 10',
+	      'IE 11'
+      ],
+      cascade: false
+    }))
+    .pipe(g.cssmin())
+    .pipe(gulp.dest(paths.less.dest)).on('error', gutil.log);
 });
