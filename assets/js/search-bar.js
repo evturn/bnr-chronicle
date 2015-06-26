3963e168cb60d307771a58d441658bfe042b852a
Evan Turner <evturn@gmail.com>
2015-06-26 17:22:18 -0400

    animation added to search bar

--- assets/js/search-bar.js
+++ assets/js/search-bar.js
@@ -1,11 +1,15 @@
 $(function() {
 	$(document).on('click', '.search-field-open', function() {
-		$('.collapsible').removeClass('hidden');
 		$('.search-launch').addClass('hidden');
+		$('.collapsible').slideToggle();
+		$('.collapsible').removeClass('hidden');
 	});
 	$(document).on('click', '.search-field-collapse', function() {
-		$('.search-launch').removeClass('hidden');
-		$('.collapsible').addClass('hidden');
+		var $self = $(this);
+		$('.collapsible').slideToggle('fast', function() {
+			$('.search-launch').removeClass('hidden');
+		});
+		$self.addClass('hidden');
 	});
 	$(document).on('click', '.search-focus', function() {
 		$('.search-input').focus();
