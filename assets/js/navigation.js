46910513dcdc7bcd546cfc62081167b6257a699e
Evan Turner <evturn@gmail.com>
2015-06-29 11:38:33 -0400

    added navigation overlay

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,5 +1,8 @@
 $(function() {
-	$(document).on('click', '.fa-bars', function() {
+	$(document).on('click', '.nav-hamburger .fa-bars', function() {
 		$('.navigation-wrapper').removeClass('hidden');
+	});
+	$(document).on('click', '.navigation-header .fa-times', function() {
+		$('.navigation-wrapper').addClass('hidden');
 	});
 });
