74351e5b5cd4b98a796644afd8eb13df020b0b52
Evan Turner <evturn@gmail.com>
2015-07-06 11:36:48 -0400

    updated server to listen on port

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express'),
-		hbs = require('./config/handlebars');
+		hbs = require('./config/handlebars'),
+		root = __dirname + '/dist';
 
 var app = express();
 
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 app.engine('hbs', hbs.engine);
+app.use(express.static(root));
+
+app.listen(3000);
