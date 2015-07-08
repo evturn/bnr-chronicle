f5900fb9a130419e65b6651da492ae53d4979fa4
Evan Turner <evturn@gmail.com>
2015-07-08 11:57:57 -0400

    added animation for navigation text

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -7,16 +7,46 @@ $(function() {
 	var $links = $('.links-container');
 
 	$(document).on('click', '.nav-hamburger', function() {
-		$wrapper
-			.removeClass('slideOutUp')
-			.removeClass('hidden');
+		
+		function openNavigation() {
+			$wrapper
+				.removeClass('slideOutUp')
+				.removeClass('hidden');
+		}
+
+			
+		function showLinks() {
+			$links
+				.removeClass('fadeOut')
+				.addClass('fadeIn')
+				.removeClass('hidden');
+			$closeBtn
+				.removeClass('fadeOut')
+				.addClass('fadeIn')
+				.removeClass('hidden');
+		}
+
+		openNavigation();
+		setTimeout(showLinks, 800);
+
 	});
 
 	$(document).on('click', '.navigation-close', function() {
-		$wrapper
-			.addClass('slideOutUp');
+
+		function hideLinks() {
+				$links
+					.addClass('fadeOut');
+				$closeBtn
+					.addClass('fadeOut');
+			}
+
+
+		function closeNavigation() {
+			$wrapper.addClass('slideOutUp');
+		}
 			
-			
+		hideLinks();
+		setTimeout(closeNavigation, 500);
 
 	});
 
